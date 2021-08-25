3b5ed77fcc5b1ae37a472a30ff139fd5156637a2
Yongcheng Wu <yc_wu1030@126.com>
2021-08-24 21:49:04 -0500

    func(CALCULATE_CS): function to calculate cs
    Add function to calculate cs
    The cs is only calculated at the level before Z decay

--- Process_Worker.py
+++ Process_Worker.py
@@ -13,7 +13,7 @@ import pickle
 import argparse
 import simplejson
 import multiprocessing
-from utilities import GENERATE_PROC
+from utilities import GENERATE_PROC, CALCULATE_CS
 
 CURDIR = os.getcwd()
 
@@ -53,3 +53,9 @@ if FLAG_GEN:
         GENERATE_PROC(SIG_TOTAL[pid],WORKDIR,UFO,YUKTYPE)
     for pid in BKG_PROCS.keys():
         GENERATE_PROC(BKG_PROCS[pid],WORKDIR,UFO)
+
+if FLAG_XEC: # Calculate Cross Section, keep the value for future reference
+    res1=CALCULATE_CS(SIG_COMPONENTS['TRI'],WORKDIR, {'MHH':550,'MHA':700,'MHp':700,'tb':1,'WHH':27.5,'WHA':35,'WHp':35},YUKTYPE)
+    res2=CALCULATE_CS(BKG_PROCS['TOTAL'],WORKDIR, {'MHH':550,'MHA':700,'MHp':700,'tb':1,'WHH':27.5,'WHA':35,'WHp':35})
+    print(res1)
+    print(res2)
--- utilities.py
+++ utilities.py
@@ -2,26 +2,63 @@ import os
 from os import listdir, getcwd, remove
 from os.path import isdir, join
 import subprocess
+import time
+from shutil import copyfile
 
 CURDIR = getcwd()
 MG5DIR = '/Users/ycwu/Workingspace/MC-Generator/MG5_aMC_v3_1_1'
 
 def GENERATE_PROC(process, WORKDIR, UFO, YUKTYPE = None):
-    proc=process['PROC']
-    nproc=len(proc)
-    name=process['NAME']
+    for tag in ['', '_Z']:
+        proc=process['PROC%s'%(tag)]
+        nproc=len(proc)
+        name=process['NAME%s'%(tag)]
+        if YUKTYPE:
+            name=process['NAME%s'%(tag)]%(YUKTYPE)
+        tmpfile=join(CURDIR,'tmp_mg5.dat')
+        PROCDIR=join(WORKDIR,name)
+        with open(tmpfile,'w') as OUTGEN:
+            OUTGEN.write('import model %s\n'%(UFO))
+            OUTGEN.write('generate %s\n'%(proc[0]))
+            if nproc > 1:
+                for i in range(1,nproc):
+                    OUTGEN.write('add process %s\n'%(proc[i]))
+            OUTGEN.write('output %s -f\n'%(PROCDIR))
+        subprocess.call('rm -rf %s'%PROCDIR,shell=True)
+        subprocess.call('%s/bin/mg5_aMC %s'%(MG5DIR,tmpfile),shell=True)
+        remove(tmpfile)
+        remove(join(CURDIR,'py.py'))
+
+def CALCULATE_CS(process, WORKDIR, PARAMS, YUKTYPE = None, SQRTS = 14):
+    name=process['NAME_Z']
     if YUKTYPE:
-        name=process['NAME']%(YUKTYPE)
-    tmpfile=join(CURDIR,'tmp_mg5.dat')
+        name=process['NAME_Z']%(YUKTYPE)
     PROCDIR=join(WORKDIR,name)
-    with open(tmpfile,'w') as OUTGEN:
-        OUTGEN.write('import model %s\n'%(UFO))
-        OUTGEN.write('generate %s\n'%(proc[0]))
-        if nproc > 1:
-            for i in range(1,nproc):
-                OUTGEN.write('add process %s\n'%(proc[i]))
-        OUTGEN.write('output %s -f\n'%(PROCDIR))
-    subprocess.call('rm -rf %s'%PROCDIR,shell=True)
-    subprocess.call('%s/bin/mg5_aMC %s'%(MG5DIR,tmpfile),shell=True)
-    remove(tmpfile)
-    remove(join(CURDIR,'py.py'))
+    tmpfile=join(PROCDIR,'xec_command.in')
+    csfile=join(PROCDIR,'xec_output.txt')
+    timetag=time.strftime("%Y%m%d_%H%M%S",time.localtime(time.time()))
+    runname='run_xec_%s'%(timetag)
+    EVEDIR=join(PROCDIR,'Events/%s'%(runname))
+    tmpfile2=join(EVEDIR,'xec_command.in')
+    csfile2=join(EVEDIR,'xec_output.txt')
+    EBEAM=SQRTS/2*1000
+    # remove madspin to avoid decay tops
+    try:
+        remove('%s/Cards/madspin_card.dat'%PROCDIR)
+    except OSError:
+        pass
+    with open(tmpfile, 'w') as OUTXEC:
+        OUTXEC.write('generate_events %s\n'%runname)
+        OUTXEC.write('0\n')
+        OUTXEC.write('set ebeam1 %f\n'%(EBEAM))
+        OUTXEC.write('set ebeam2 %f\n'%(EBEAM))
+        OUTXEC.write('set nevents 50000\n')
+        OUTXEC.write('set no_parton_cut\n')
+        for param in PARAMS.keys():
+            OUTXEC.write('set %s %f\n'%(param,PARAMS[param]))
+        OUTXEC.write('0\n')
+    subprocess.call('%s/bin/madevent %s > %s'%(PROCDIR,tmpfile,csfile),shell=True)
+    res=subprocess.check_output('awk \'$1=="Cross-section" {print $3}\' %s'%(csfile),shell=True)
+    copyfile(tmpfile,tmpfile2)
+    copyfile(csfile,csfile2)
+    return float(res)
