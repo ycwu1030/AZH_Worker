624a8331d071d655bade85d32e6cd839e1b8f677
Yongcheng Wu <yc_wu1030@126.com>
2021-09-04 21:12:42 -0500

    func(Calculate CS Script): Add script for CS

---
+++ Calculate_CS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p express
+#SBATCH -t 1:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+##SBATCH --mail-user=ywu@okstate.edu
+##SBATCH --mail-type=end
+
+python Process_Worker.py -i Processes/AZH.json -d -x
