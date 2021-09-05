732a233925136a0faf977cd992e7a645ae4b9138
Yongcheng Wu <yc_wu1030@126.com>
2021-09-05 10:02:10 -0500

    impr(PreAnalysis): update script for PreAnalysis

--- PreAnalysis.sh
+++ PreAnalysis.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-#SBATCH -p express
-#SBATCH -t 1:00:00
+#SBATCH -p batch
+#SBATCH -t 24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 ##SBATCH --mail-user=ywu@okstate.edu
 ##SBATCH --mail-type=end
 
-paramfile=$1
-python Process_Worker.py -i Processes/AZH.json -a -p $paramfile
+files=("param_signal_202108290019_0x005.json" "param_signal_202108290019_0x010.json" "param_signal_202    108290019_0x025.json" "param_signal_202108290019_0x050.json" "param_signal_202108290019_0x100.json" "p    aram_signal_202108290019_0x250.json")
+for file in ${files[@]}
+  do
+    python Process_Worker.py -i Processes/AZH.json -a -s -p DATADIR/$file&
+done
+
+python Process_Worker.py -i Processes/AZH.json -a -p DATADIR/param_bkg.json&
+wait
