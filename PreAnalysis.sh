5e8602201d0ac4ad91e8e04b4d5b4db7d8a6dfcb
Yongcheng Wu <yc_wu1030@126.com>
2021-09-05 10:05:42 -0500

    fix(PreAnalysis): typo in file names

--- PreAnalysis.sh
+++ PreAnalysis.sh
@@ -6,7 +6,7 @@
 ##SBATCH --mail-user=ywu@okstate.edu
 ##SBATCH --mail-type=end
 
-files=("param_signal_202108290019_0x005.json" "param_signal_202108290019_0x010.json" "param_signal_202    108290019_0x025.json" "param_signal_202108290019_0x050.json" "param_signal_202108290019_0x100.json" "p    aram_signal_202108290019_0x250.json")
+files=("param_signal_202108290019_0x005.json" "param_signal_202108290019_0x010.json" "param_signal_202108290019_0x025.json" "param_signal_202108290019_0x050.json" "param_signal_202108290019_0x100.json" "param_signal_202108290019_0x250.json")
 for file in ${files[@]}
   do
     python Process_Worker.py -i Processes/AZH.json -a -s -p DATADIR/$file&
