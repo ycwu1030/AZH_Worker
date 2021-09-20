e8f72003f192fc87d9ca17ddd8f90721092c390c
Yongcheng Wu <yc_wu1030@126.com>
2021-09-20 09:55:21 -0500

    impr(Generate_Events): Update script to generate events for component of the signal processes

--- Generate_Events.sh
+++ Generate_Events.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 72:00:00
+#SBATCH -t 96:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 ##SBATCH --mail-user=ywu@okstate.edu
 ##SBATCH --mail-type=end
 
 paramfile=$1
-python Process_Worker.py -i Processes/AZH.json -d -s -n 4 -p $paramfile
+python Process_Worker.py -i Processes/AZH.json -d -s -sc -n 2 -p $paramfile
