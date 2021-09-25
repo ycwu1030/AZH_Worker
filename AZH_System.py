e003b2c6c95046beff0bb04b3fcfe3f24b47c573
Yongcheng Wu <yc_wu1030@126.com>
2021-09-25 13:38:35 -0500

    func(AZH_System): Add AZH_System class to handle the functions in such system
    Add AZH_System class for:
      Generate Process Folders
      Calculate CS
      Generate Events
      Scan over parameters

---
+++ AZH_System.py
@@ -0,0 +1,158 @@
+from Process_Worker import BKG_PROCS
+import os
+import json
+from copy import copy, deepcopy
+from os.path import dirname, abspath, join
+from MG_Runner import MG_RUNNER
+
+
+class AZH_System(object):
+    __BASE_DIR__ = dirname(abspath(__file__))
+    __MG5_DIR__ = join(__BASE_DIR__, 'MG5DIR/MG5_aMC_v3_1_1')
+
+    def __init__(self, INFOFILE, DEBUG=False):
+        with open(INFOFILE, 'r') as f:
+            self.INFO = json.loads(f.read())
+
+        self.YUKTYPE = self.INFO['TYPE']
+        self.UFO = self.INFO['UFO'] % (self.YUKTYPE)
+        self.SQRTS = self.INFO['SQRTS']
+        self.DECAYS = self.INFO['DECAY']
+        self.SIG_COMPONENTS = self.INFO['SIGNAL_COMPONENT']
+        self.SIG_TOTAL = self.INFO['SIGNAL_TOTAL']
+        self.BKG_PROCS = self.INFO['BACKGROUND']
+
+        self.WORK_DIR = join(self.__BASE_DIR__, 'WORKDIR')
+        if not os.path.exists(self.WORK_DIR):
+            os.makedirs(self.WORK_DIR)
+        self.DATA_DIR = join(self.__BASE_DIR__, 'DATADIR')
+        if not os.path.exists(self.DATA_DIR):
+            os.makedirs(self.DATA_DIR)
+        self.MG_HANDLER = MG_RUNNER(self.__MG5_DIR__, self.WORK_DIR, DEBUG)
+        self.CARDS = {
+            'DELPHES': join(self.__BASE_DIR__, 'tmp_cards/delphes_card.dat'),
+            'PYTHIA8': join(self.__BASE_DIR__, 'tmp_cards/pythia8_card.dat')
+        }
+        self.CARDS_DECAY = {
+            '3l': join(self.__BASE_DIR__, 'tmp_cards/madspin_card_semilep.dat'),
+            '4l': join(self.__BASE_DIR__, 'tmp_cards/madspin_card_dilepton.dat')}
+        self.INPUT_PARAMETERS = {}
+        self.RESULT_PARAMETERS = {}
+
+    def Generate_Process_Folders(self):
+        ALL_PROC_LIST = [self.SIG_COMPONENTS, self.SIG_TOTAL, self.BKG_PROCS]
+        for PROC_CAT_ID in range(len(ALL_PROC_LIST)):
+            PROC_CAT = ALL_PROC_LIST[PROC_CAT_ID]
+            for pid in PROC_CAT.keys():
+                process = PROC_CAT[pid]
+                for tag in ['', '_Z']:
+                    NAME = process['NAME%s' % (
+                        tag)] if PROC_CAT_ID is 2 else process['NAME%s' % (tag)] % (self.YUKTYPE)
+                    UFO = self.UFO
+                    PROCS = process['PROC%s' % (tag)]
+                    self.MG_HANDLER.Generate_Process(
+                        {'NAME': NAME, 'UFO': UFO, 'PROCS': PROCS})
+
+    def Generate_Events(self, PROCNAME, PARAMS, CHAN):
+
+        # * Prepare the Cards for Event Generation
+        CARDS = copy(self.CARDS)
+        CARDS['MADSPIN'] = self.CARDS_DECAY[CHAN]
+
+        # * Prepare the PARAMETERS
+        PARAMS['CHAN'] = CHAN
+
+        # * Run the MadEvent
+        cs, filename = self.MG_HANDLER.Run_MadEvent(
+            PROCNAME, self.DATA_DIRT, PARAMS, CARDS)
+        return filename
+
+    def Calculate_CS(self, PROCNAME, PARAMS):
+
+        PARAMS['CHAN'] = 'cs'
+
+        cs, filename = self.MG_HANDLER.Run_MadEvent(
+            PROCNAME, self.DATA_DIR, PARAMS)
+        return cs
+
+    def Read_Parameters(self, PARAMFILE):
+        with open(PARAMFILE, 'r') as f:
+            self.INPUT_PARAMETERS = json.load(f)
+        TMPNAME = os.path.basename(PARAMFILE)
+        RESULTFILE = join(self.DATA_DIR, TMPNAME)
+        if os.path.isfile(RESULTFILE):
+            with open(RESULTFILE, 'r') as f:
+                self.RESULT_PARAMETERS = json.load(f)
+        else:
+            self.RESULT_PARAMETERS = {}
+
+    def Dump_Parameters(self, PARAMFILE):
+        TMPNAME = os.path.basename(PARAMFILE)
+        RESULTFILE = join(self.DATA_DIR, TMPNAME)
+        with open(RESULTFILE, 'w') as f:
+            json.dump(self.RESULT_PARAMETERS, f, sort_keys=True, indent=4)
+
+    def Prepare_Parameters(self, param_key, proc_key, chan_id):
+
+        PARAM = copy(self.INPUT_PARAMETERS[param_key])
+        if param_key in self.RESULT_PARAMETERS.keys():
+            PARAM = copy(self.RESULT_PARAMETERS[param_key])
+        RUN_CS = False
+        if 'CS' not in PARAM.keys():
+            PARAM['CS'] = {}
+            RUN_CS = True
+        elif proc_key not in PARAM['CS'].keys():
+            PARAM['CS'][proc_key] = 0
+            RUN_CS = True
+        N_ROOT_FILES = 0
+        if 'ROOT' not in PARAM.keys():
+            PARAM['ROOT'] = {}
+
+        if chan_id not in PARAM['ROOT'].keys():
+            PARAM['ROOT'][chan_id] = {}
+
+        if proc_key not in PARAM['ROOT'][chan_id].keys():
+            PARAM['ROOT'][chan_id][proc_key] = []
+
+        N_ROOT_FILES = len(PARAM['ROOT'][chan_id][proc_key])
+
+        return RUN_CS, N_ROOT_FILES, PARAM
+
+    def Scan_Parameters(self, PARAMFILE, NRUNS, FLAG_SIG=True, FLAG_SIG_COMPONENT=True, CHAN='3l'):
+
+        PROCS_TO_BE_RUN = {}
+        if FLAG_SIG:
+            if FLAG_SIG_COMPONENT:
+                PROCS_TO_BE_RUN = self.SIG_COMPONENTS
+            else:
+                PROCS_TO_BE_RUN = self.SIG_TOTAL
+        else:
+            PROCS_TO_BE_RUN = BKG_PROCS
+
+        self.Read_Parameters(PARAMFILE)
+
+        for param_key in self.INPUT_PARAMETERS.keys():
+            for pid in PROCS_TO_BE_RUN.keys():
+                # * Checking for the specific parameter and the process and the channel, whether we need to calculate the CS, and how many root files we already have and prepare the parameters
+                RUN_CS, N_ROOT_FILES, PARAM = self.Prepare_Parameters(
+                    param_key, pid, CHAN)
+
+                USED_PARAM = copy(PARAM)
+                if RUN_CS:
+                    PROCNAME = PROCS_TO_BE_RUN[pid]['NAME_Z']
+                    if FLAG_SIG:
+                        PROCNAME = PROCNAME % (self.YUKTYPE)
+                    PARAM['CS'][pid], _ = self.Calculate_CS(
+                        PROCNAME, USED_PARAM)
+                    self.RESULT_PARAMETERS[param_key] = PARAM
+                    self.Dump_Parameters(PARAMFILE)
+
+                for ntimes in range(N_ROOT_FILES, NRUNS):
+                    PROCNAME = PROCS_TO_BE_RUN[pid]['NAME']
+                    if FLAG_SIG:
+                        PROCNAME = PROCNAME % (self.YUKTYPE)
+                    _, root_file_name = self.Generate_Events(
+                        PROCNAME, USED_PARAM, CHAN)
+                    PARAM['ROOT'][CHAN][pid].append(root_file_name)
+                    self.RESULT_PARAMETERS[param_key] = PARAM
+                    self.Dump_Parameters(PARAMFILE)
--- MG_Runner.py
+++ MG_Runner.py
@@ -5,11 +5,13 @@ import subprocess
 import time
 from shutil import copyfile
 
+
 class MG_RUNNER(object):
 
-    def __init__(self, MG5DIR, WORKDIR):
+    def __init__(self, MG5DIR, WORKDIR, DEBUG=False):
         self.MG5_DIR = MG5DIR
         self.WORK_DIR = WORKDIR
+        self.DEBUG = DEBUG
 
     def Generate_Process(self, PROC):
         '''
@@ -26,40 +28,42 @@ class MG_RUNNER(object):
         UFO = PROC['UFO']
         proc = PROC['PROCS']
         CURDIR = getcwd()
-        tmpfile = join(CURDIR, 'tmp_%s.dat'%(name))
-        PROCDIR = join(self.WORK_DIR,name)
+        tmpfile = join(CURDIR, 'tmp_%s.dat' % (name))
+        PROCDIR = join(self.WORK_DIR, name)
         nproc = len(proc)
-        with open(tmpfile,'w') as TMP:
-            TMP.write('import model %s\n'%(UFO))
-            TMP.write('generate %s\n'%(proc[0]))
-            for i in range(1,nproc):
-                TMP.write('add process %s\n'%(proc[i]))
-            TMP.write('output %s -f\n'%(PROCDIR))
-        subprocess.call('rm -rf %s'%(PROCDIR),shell=True)
-        subprocess.call('python %s/bin/mg5_aMC %s'%(self.MG5_DIR,tmpfile),shell=True)
+        with open(tmpfile, 'w') as TMP:
+            TMP.write('import model %s\n' % (UFO))
+            TMP.write('generate %s\n' % (proc[0]))
+            for i in range(1, nproc):
+                TMP.write('add process %s\n' % (proc[i]))
+            TMP.write('output %s -f\n' % (PROCDIR))
+        subprocess.call('rm -rf %s' % (PROCDIR), shell=True)
+        if not self.DEBUG:
+            subprocess.call('python %s/bin/mg5_aMC %s' %
+                            (self.MG5_DIR, tmpfile), shell=True)
 
-    def Prepare_Cards(self,procdir,CARDS):
-        CARDDIR=join(procdir,'Cards')
+    def Prepare_Cards(self, procdir, CARDS):
+        CARDDIR = join(procdir, 'Cards')
         if CARDS:
-            copyfile(CARDS['MADSPIN'],join(CARDDIR,'madspin_card.dat'))
-            copyfile(CARDS['DELPHES'],join(CARDDIR,'delphes_card.dat'))
-            copyfile(CARDS['PYTHIA8'],join(CARDDIR,'pythia8_card.dat'))
+            copyfile(CARDS['MADSPIN'], join(CARDDIR, 'madspin_card.dat'))
+            copyfile(CARDS['DELPHES'], join(CARDDIR, 'delphes_card.dat'))
+            copyfile(CARDS['PYTHIA8'], join(CARDDIR, 'pythia8_card.dat'))
         else:
-        # remove madspin to avoid decay tops
+            # remove madspin to avoid decay tops
             try:
-                remove('%s/madspin_card.dat'%CARDDIR)
+                remove('%s/madspin_card.dat' % CARDDIR)
             except OSError:
                 pass
             try:
-                remove('%s/delphes_card.dat'%CARDDIR)
+                remove('%s/delphes_card.dat' % CARDDIR)
             except OSError:
                 pass
             try:
-                remove('%s/pythia8_card.dat'%CARDDIR)
+                remove('%s/pythia8_card.dat' % CARDDIR)
             except OSError:
                 pass
 
-    def Run_MadEvent(self,PROCNAME,DATADIR,PARAMS,CARDS = None,SQRTS = 14):
+    def Run_MadEvent(self, PROCNAME, DATADIR, PARAMS, CARDS=None, SQRTS=14):
         '''
         PROCNAME: the name of the process, used to identify the folder.
         DATADIR: The place to keep the logs and the events.
@@ -73,12 +77,12 @@ class MG_RUNNER(object):
         paramid = PARAMS['ID']
         chanid = PARAMS['CHAN']
         # * Prepare the running folder
-        PROCDIR_ORI = join(self.WORK_DIR,PROCNAME)
-        PROCDIR = join(self.WORK_DIR,"%s_%s_%s"%(PROCNAME,paramid,chanid))
-        subprocess.call('cp -r %s %s'%(PROCDIR_ORI, PROCDIR),shell=True)
+        PROCDIR_ORI = join(self.WORK_DIR, PROCNAME)
+        PROCDIR = join(self.WORK_DIR, "%s_%s_%s" % (PROCNAME, paramid, chanid))
+        subprocess.call('cp -r %s %s' % (PROCDIR_ORI, PROCDIR), shell=True)
 
         # * Prepare the auxillary cards
-        self.Prepare_Cards(PROCDIR,CARDS)
+        self.Prepare_Cards(PROCDIR, CARDS)
 
         # * Check we are generating events or calculate the cs
         if CARDS:
@@ -87,46 +91,56 @@ class MG_RUNNER(object):
             log_prefix = 'xec'
 
         # * Prepare the Log directory
-        timetag=time.strftime("%Y%m%d_%H%M%S",time.localtime(time.time()))
-        PROC_DATA_DIR = join(DATADIR,paramid)
-        PROC_LOG_DIR=join(PROC_DATA_DIR,'logs')
+        timetag = time.strftime("%Y%m%d_%H%M%S", time.localtime(time.time()))
+        PROC_DATA_DIR = join(DATADIR, paramid)
+        PROC_LOG_DIR = join(PROC_DATA_DIR, 'logs')
         if not os.path.exists(PROC_DATA_DIR):
             os.makedirs(PROC_DATA_DIR)
         if not os.path.exists(PROC_LOG_DIR):
             os.makedirs(PROC_LOG_DIR)
-        CMD_FILE_NAME='%s_command_%s_%s_%s_%s.txt'%(log_prefix,PROCNAME,paramid,chanid,timetag)
-        LOG_FILE_NAME='%s_output_%s_%s_%s_%s.txt'%(log_prefix,PROCNAME,paramid,chanid,timetag)
-        CMDFILE=join(PROCDIR,CMD_FILE_NAME)
-        LOGFILE=join(PROCDIR,LOG_FILE_NAME)
-        CMDFILE_KEEP=join(PROC_LOG_DIR,CMD_FILE_NAME)
-        LOGFILE_KEEP=join(PROC_LOG_DIR,LOG_FILE_NAME)
+        CMD_FILE_NAME = '%s_command_%s_%s_%s_%s.txt' % (
+            log_prefix, PROCNAME, paramid, chanid, timetag)
+        LOG_FILE_NAME = '%s_output_%s_%s_%s_%s.txt' % (
+            log_prefix, PROCNAME, paramid, chanid, timetag)
+        CMDFILE = join(PROCDIR, CMD_FILE_NAME)
+        LOGFILE = join(PROCDIR, LOG_FILE_NAME)
+        CMDFILE_KEEP = join(PROC_LOG_DIR, CMD_FILE_NAME)
+        LOGFILE_KEEP = join(PROC_LOG_DIR, LOG_FILE_NAME)
 
         # * Prepare the running command
         EBEAM = SQRTS/2*1000
-        run_name = 'run_%s_%s_%s'%(log_prefix,paramid,timetag)
+        run_name = 'run_%s_%s_%s' % (log_prefix, paramid, timetag)
         MODEL_PARAMS = PARAMS['PARAM']
         with open(CMDFILE, 'w') as TMP:
-            TMP.write('generate_events %s\n'%(run_name))
+            TMP.write('generate_events %s\n' % (run_name))
             TMP.write('0\n')
-            TMP.write('set ebeam1 %f\n'%(EBEAM))
-            TMP.write('set ebeam2 %f\n'%(EBEAM))
+            TMP.write('set ebeam1 %f\n' % (EBEAM))
+            TMP.write('set ebeam2 %f\n' % (EBEAM))
             TMP.write('set nevents 50000\n')
-            TMP.write('set iseed %d\n'%(int(time.time())%10000000))
+            TMP.write('set iseed %d\n' % (int(time.time()) % 10000000))
             TMP.write('set no_parton_cut\n')
             for param in MODEL_PARAMS.keys():
-                TMP.write('set %s %f\n'%(param,MODEL_PARAMS[param]))
+                TMP.write('set %s %f\n' % (param, MODEL_PARAMS[param]))
             TMP.write('0\n')
 
         # * Run the MadEvent
-        subprocess.call('python %s/bin/madevent %s > %s'%(PROCDIR,CMDFILE,LOGFILE),shell=True)
-        res=subprocess.check_output('awk \'$1=="Cross-section" {print $3}\' %s'%(LOGFILE),shell=True)
+        if not self.DEBUG:
+            subprocess.call('python %s/bin/madevent %s > %s' %
+                            (PROCDIR, CMDFILE, LOGFILE), shell=True)
+            res = subprocess.check_output(
+                'awk \'$1=="Cross-section" {print $3}\' %s' % (LOGFILE), shell=True)
 
-        # * Moving the events and logs
-        copyfile(CMDFILE,CMDFILE_KEEP)
-        copyfile(LOGFILE,LOGFILE_KEEP)
-        root_file_name = 'N.A.'
-        if CARDS:
-            root_file_name = 'delphes_%s_%s_%s_%s.root'%(PROCNAME,paramid,chanid,timetag)
-            copyfile(join(PROCDIR,'Events/%s_decayed_1/tag_1_delphes_events.root'%(PROCNAME)),join(PROC_DATA_DIR,root_file_name))
-        subprocess.call('rm -rf %s'%(PROCDIR),shell=True)
-        return float(res),root_file_name
+            # * Moving the events and logs
+            copyfile(CMDFILE, CMDFILE_KEEP)
+            copyfile(LOGFILE, LOGFILE_KEEP)
+            root_file_name = 'N.A.'
+            if CARDS:
+                root_file_name = 'delphes_%s_%s_%s_%s.root' % (
+                    PROCNAME, paramid, chanid, timetag)
+                copyfile(join(PROCDIR, 'Events/%s_decayed_1/tag_1_delphes_events.root' %
+                              (PROCNAME)), join(PROC_DATA_DIR, root_file_name))
+        else:
+            res = 0
+            root_file_name = 'delphes_test.root'
+        subprocess.call('rm -rf %s' % (PROCDIR), shell=True)
+        return float(res), root_file_name
