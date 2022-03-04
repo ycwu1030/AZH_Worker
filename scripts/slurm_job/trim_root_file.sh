d12ab97ef6b99bc20d59e3fffa751f25db71e92a
Yongcheng Wu <yc_wu1030@126.com>
2022-03-03 21:56:46 -0600

    update trim root script

--- scripts/slurm_job/trim_root_file.sh
+++ scripts/slurm_job/trim_root_file.sh
@@ -6,5 +6,6 @@
 ##SBATCH --mail-user=ywu@okstate.edu
 ##SBATCH --mail-type=end
 
+source /opt/cern-root/6.22.06/prebuilt/bin/thisroot.sh
 module load anaconda3/2020.07
 python -c "from tools import TrimRootFile as TRF; TRF.TrimRootFileAll(\"DATADIR_TMP\")"
