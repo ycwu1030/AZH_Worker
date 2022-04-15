821dddfc524b83d152893ce2a484e09909535613
Yongcheng Wu <yc_wu1030@126.com>
2022-04-15 09:18:10 +0800

    update combine bkg root file script

--- tools/Combine_BKG_Root.py
+++ tools/Combine_BKG_Root.py
@@ -25,22 +25,24 @@ def Combine_Background(INFOFILE, PARAMDIR):
     cs_number = 0
     root_file_list_combined = []
     for file in PARAMFILES:
-        param_individual = {}
+        param_infile = {}
         with open(join(PARAMDIR, file)) as f:
-            param_individual = json.loads(f.read())
-        cs = param_individual['CS']['bkg_total']['DEFAULT']
-        cs_accumulated += cs
-        cs_number += 1
-        root_file_list = param_individual['ROOT']['3l']['bkg_total']['DEFAULT']
-        root_dir = join(DATA_DIR, 'BKG_PARAM', str(
-            param_individual['ID']), 'bkg_total')
-        for file in root_file_list:
-            file_name, ext = splitext(file)
-            file_out = file_name + '_' + str(cs_number) + ext
-            copyfile(join(root_dir, file), join(OUTPUT_DIR, file_out))
-            root_file_list_combined.append(file_out)
+            param_infile = json.loads(f.read())
+        for param_key in param_infile.keys():
+            param_individual = param_infile[param_key]
+            cs = param_individual['CS']['bkg_total']['DEFAULT']
+            cs_accumulated += cs
+            cs_number += 1
+            root_file_list = param_individual['ROOT']['3l']['bkg_total']['DEFAULT']
+            root_dir = join(DATA_DIR, 'BKG_PARAM', str(
+                param_individual['ID']), 'bkg_total')
+            for file in root_file_list:
+                file_name, ext = splitext(file)
+                file_out = file_name + '_' + str(cs_number) + ext
+                copyfile(join(root_dir, file), join(OUTPUT_DIR, file_out))
+                root_file_list_combined.append(file_out)
     cs_averaged = cs_accumulated/cs_number
-    OUTPUT_PARAM['CS']['bkg_total']['DEFAULT'] = cs_averaged
-    OUTPUT_PARAM['ROOT']['3l']['bkg_total']['DEFAULT'] = root_file_list_combined
+    OUTPUT_PARAM['BKG_COMB']['CS']['bkg_total']['DEFAULT'] = cs_averaged
+    OUTPUT_PARAM['BKG_COMB']['ROOT']['3l']['bkg_total']['DEFAULT'] = root_file_list_combined
     with open(OUTPUT_PARAM_FILE, 'w') as f:
         json.dump(OUTPUT_PARAM, f, sort_keys=True, indent=4)
