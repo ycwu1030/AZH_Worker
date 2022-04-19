122c1b3468478da5f25af934b04a51d08a6ba662
Yongcheng Wu <yc_wu1030@126.com>
2022-04-19 10:22:14 +0800

    type-II model with quark prime, w/o new coupling order

---
+++ models/2HDM_CPC_Type-II_quarkprime_NLO_cba_UFO/coupling_orders.py
@@ -0,0 +1,36 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 12.3.1 for Mac OS X ARM (64-bit) (July 8, 2021)
+# Date: Tue 15 Feb 2022 11:02:46
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name='QCD',
+                    expansion_order=99,
+                    hierarchy=1,
+                    perturbative_expansion=1)
+
+QED = CouplingOrder(name='QED',
+                    expansion_order=99,
+                    hierarchy=2)
+
+ZFF = CouplingOrder(name='ZFF',
+                    expansion_order=99,
+                    hierarchy=2)
+
+ZSS = CouplingOrder(name='ZSS',
+                    expansion_order=99,
+                    hierarchy=2)
+
+ZZHH = CouplingOrder(name='ZZHH',
+                     expansion_order=99,
+                     hierarchy=2)
+
+TTHA = CouplingOrder(name='TTHA',
+                     expansion_order=99,
+                     hierarchy=2)
+
+TTHH = CouplingOrder(name='TTHH',
+                     expansion_order=99,
+                     hierarchy=2)
